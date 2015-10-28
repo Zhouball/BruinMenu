3c0b4cca59991b29d8b7cf9c5fdea17a6134676c
Allen Zhou <Zhouball@gmail.com>
2015-10-28 01:29:39 -0700

    Added credits page

---
+++ app/src/main/java/zhou/allen/bruinmenu/CreditsActivity.java
@@ -0,0 +1,36 @@
+package zhou.allen.bruinmenu;
+
+import android.support.v4.app.NavUtils;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+public class CreditsActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_credits);
+    }
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.credits, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.home:
+                //setContentView(R.layout.activity_main);
+                finish();
+            default:
+                return super.onOptionsItemSelected(item);
+        }
+    }
+}
--- app/src/main/java/zhou/allen/bruinmenu/MainActivity.java
+++ app/src/main/java/zhou/allen/bruinmenu/MainActivity.java
@@ -27,6 +27,8 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.widget.Toast;
 
 import java.util.Calendar;
@@ -90,6 +92,28 @@ public class MainActivity extends AppCompatActivity implements MaterialTabListen
                 refreshContent();
             }
         });
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+        if (id == R.id.action_credits) {
+            Intent i = new Intent(this, CreditsActivity.class);
+            startActivity(i);
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
     private void refreshContent(){
