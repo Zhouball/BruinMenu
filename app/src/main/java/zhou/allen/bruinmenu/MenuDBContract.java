3e9d9cd9a133f8899a77906287c64451dcbb8166
Allen Zhou <Zhouball@gmail.com>
2015-11-04 02:19:17 -0800

    Updated database

--- app/src/main/java/zhou/allen/bruinmenu/MenuDBContract.java
+++ app/src/main/java/zhou/allen/bruinmenu/MenuDBContract.java
@@ -13,22 +13,45 @@ public final class MenuDBContract {
     private static final String TEXT_TYPE = " TEXT";
     private static final String COMMA_SEP = ",";
 
-
     public MenuDBContract() {
     }
 
     public static abstract class MenuEntry implements BaseColumns {
         public static final String TABLE_NAME = "menu";
         public static final String COLUMN_NAME_ITEM = "menuitem";
-        public static final String COLUMN_NAME_LOC = "loc";
+        public static final String COLUMN_NAME_KITCHEN = "kitchen";
+
+        public static final String CREATE_TABLE = "CREATE TABLE " +
+                TABLE_NAME + "(" +
+                _ID + " INTEGER PRIMARY KEY," +
+                COLUMN_NAME_ITEM + TEXT_TYPE + COMMA_SEP +
+                COLUMN_NAME_KITCHEN + TEXT_TYPE + " )";
+        public static final String DELETE_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME;
+    }
+
+    public static abstract class HallEntry implements BaseColumns {
+        public static final String TABLE_NAME = "halls";
+        public static final String COLUMN_NAME_ITEM = "hall";
         public static final String COLUMN_NAME_MEALTIME = "mealtime";
 
         public static final String CREATE_TABLE = "CREATE TABLE " +
                 TABLE_NAME + "(" +
                 _ID + " INTEGER PRIMARY KEY," +
                 COLUMN_NAME_ITEM + TEXT_TYPE + COMMA_SEP +
-                COLUMN_NAME_LOC + TEXT_TYPE + COMMA_SEP +
                 COLUMN_NAME_MEALTIME + TEXT_TYPE + " )";
+        public static final String DELETE_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME;
+    }
+
+    public static abstract class KitchenEntry implements BaseColumns {
+        public static final String TABLE_NAME = "kitchens";
+        public static final String COLUMN_NAME_ITEM = "kitchen";
+        public static final String COLUMN_NAME_HALL = "hall";
+
+        public static final String CREATE_TABLE = "CREATE TABLE " +
+                TABLE_NAME + "(" +
+                _ID + " INTEGER PRIMARY KEY," +
+                COLUMN_NAME_ITEM + TEXT_TYPE + COMMA_SEP +
+                COLUMN_NAME_HALL + TEXT_TYPE + " )";
         public static final String DELETE_TABLE = "DROP TABLE IF EXISTS " + TABLE_NAME;
     }
 }
--- app/src/main/java/zhou/allen/bruinmenu/MenuDBHelper.java
+++ app/src/main/java/zhou/allen/bruinmenu/MenuDBHelper.java
@@ -23,29 +23,102 @@ public class MenuDBHelper extends SQLiteOpenHelper {
     // Method is called during creation of the database
     @Override
     public void onCreate(SQLiteDatabase db) {
+        db.execSQL(MenuDBContract.KitchenEntry.CREATE_TABLE);
+        db.execSQL(MenuDBContract.HallEntry.CREATE_TABLE);
         db.execSQL(MenuDBContract.MenuEntry.CREATE_TABLE);
     }
 
     // Method is called during an upgrade of the database
     @Override
     public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+        db.execSQL(MenuDBContract.KitchenEntry.DELETE_TABLE);
+        db.execSQL(MenuDBContract.HallEntry.DELETE_TABLE);
         db.execSQL(MenuDBContract.MenuEntry.DELETE_TABLE);
         onCreate(db);
     }
 
     /* Access database */
-
+    /*
     public List<String> getEntryByLocAndMealTime(String loc,String mealTime) {
         List<String> returnList = new ArrayList<String>();
 
         String selectQuery = "SELECT " + MenuDBContract.MenuEntry.COLUMN_NAME_ITEM + " FROM " + MenuDBContract.MenuEntry.TABLE_NAME +
                 " WHERE " + MenuDBContract.MenuEntry.COLUMN_NAME_LOC + "='" + loc +
                 "' AND " + MenuDBContract.MenuEntry.COLUMN_NAME_MEALTIME + "='" + mealTime + "'";
-        /*
-        SELECT menuitem
-        FROM menu
-        WHERE loc='String loc' AND mealtime='String mealTime'
-         */
+
+
+        Log.e(LOG, selectQuery);
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor c = db.rawQuery(selectQuery, null);
+
+        // looping through all rows and adding to list
+        if (c.moveToFirst()) {
+            do {
+                String s = c.getString(c.getColumnIndex(MenuDBContract.MenuEntry.COLUMN_NAME_ITEM));
+
+                // adding to list
+                returnList.add(s);
+            } while (c.moveToNext());
+        }
+        c.close();
+        return returnList;
+    }
+    */
+
+    public List<String> getHallsByMealTime(String mealTime) {
+        List<String> returnList = new ArrayList<String>();
+
+        String selectQuery = "SELECT " + MenuDBContract.HallEntry.COLUMN_NAME_ITEM + " FROM " + MenuDBContract.HallEntry.TABLE_NAME +
+                " WHERE " + MenuDBContract.HallEntry.COLUMN_NAME_MEALTIME + "='" + mealTime + "'";
+
+        Log.e(LOG, selectQuery);
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor c = db.rawQuery(selectQuery, null);
+
+        // looping through all rows and adding to list
+        if (c.moveToFirst()) {
+            do {
+                String s = c.getString(c.getColumnIndex(MenuDBContract.HallEntry.COLUMN_NAME_ITEM));
+
+                // adding to list
+                returnList.add(s);
+            } while (c.moveToNext());
+        }
+        c.close();
+        return returnList;
+    }
+
+    public List<String> getKitchensByHall(String hall) {
+        List<String> returnList = new ArrayList<String>();
+
+        String selectQuery = "SELECT " + MenuDBContract.KitchenEntry.COLUMN_NAME_ITEM + " FROM " + MenuDBContract.KitchenEntry.TABLE_NAME +
+                " WHERE " + MenuDBContract.KitchenEntry.COLUMN_NAME_HALL + "='" + hall + "'";
+
+        Log.e(LOG, selectQuery);
+
+        SQLiteDatabase db = this.getReadableDatabase();
+        Cursor c = db.rawQuery(selectQuery, null);
+
+        // looping through all rows and adding to list
+        if (c.moveToFirst()) {
+            do {
+                String s = c.getString(c.getColumnIndex(MenuDBContract.KitchenEntry.COLUMN_NAME_ITEM));
+
+                // adding to list
+                returnList.add(s);
+            } while (c.moveToNext());
+        }
+        c.close();
+        return returnList;
+    }
+
+    public List<String> getMenuItemsByKitchen(String kitchen) {
+        List<String> returnList = new ArrayList<String>();
+
+        String selectQuery = "SELECT " + MenuDBContract.MenuEntry.COLUMN_NAME_ITEM + " FROM " + MenuDBContract.MenuEntry.TABLE_NAME +
+                " WHERE " + MenuDBContract.MenuEntry.COLUMN_NAME_KITCHEN + "='" + kitchen + "'";
 
         Log.e(LOG, selectQuery);
 
